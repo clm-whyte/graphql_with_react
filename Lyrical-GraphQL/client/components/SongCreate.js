6c86f6ecb0212385b97c2f249afc7f56dc41c648
ciw-administrate <ciw@getadministrate.com>
2020-04-30 10:44:38 +0100

    add Song Title to the SongCreate form

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -3,10 +3,23 @@ import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
 class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { title: "" };
+  }
+
   render() {
     return (
       <div>
-        <form></form>
+        <h3>Create a New Song</h3>
+        <form>
+          <label>Song Title:</label>
+          <input
+            onChange={(event) => this.setState({ title: event.target.value })}
+            value={this.state.title}
+          />
+        </form>
       </div>
     );
   }
