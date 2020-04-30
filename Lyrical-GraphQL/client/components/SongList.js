bdb26ddf219eda64251b743ec381ca50a57da35b
ciw-administrate <ciw@getadministrate.com>
2020-04-30 15:22:38 +0100

    create Add Song button

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
+import { Link } from "react-router";
 
 class Songlist extends Component {
   renderSongs() {
@@ -18,7 +19,14 @@ class Songlist extends Component {
       return <div>Loading...</div>;
     }
 
-    return <ul className="collection">{this.renderSongs()}</ul>;
+    return (
+      <div>
+        <ul className="collection">{this.renderSongs()}</ul>;
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    );
   }
 }
 
