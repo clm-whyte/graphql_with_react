c95c8ed5010c33cac42b26af2e6e8748058de90f
ciw-administrate <ciw@getadministrate.com>
2020-04-30 16:42:39 +0100

    hook up Delete Song mutation, refetch SongList on delete, move Delete icon to the far right and add pointer on hover

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 import { Link } from "react-router";
 import query from "../queries/fetchSongs";
 
 class Songlist extends Component {
+  onSongDelete(id) {
+    this.props
+      .mutate({ variables: { id } })
+      .then(() => this.props.data.refetch());
+  }
+
   renderSongs() {
-    return this.props.data.songs.map((song) => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          {title}
+          <i className="material-icons" onClick={() => this.onSongDelete(id)}>
+            delete
+          </i>
         </li>
       );
     });
