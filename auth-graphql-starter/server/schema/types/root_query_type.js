29fffcf5841b24cf57b06d069579e1d2ca033637
ciw-administrate <ciw@getadministrate.com>
2020-05-12 11:43:25 +0100

    initial commit of Auth project

--- auth-graphql-starter/server/schema/types/root_query_type.js
+++ auth-graphql-starter/server/schema/types/root_query_type.js
@@ -1,11 +1,8 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLID } = graphql;
+const { GraphQLObjectType } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
-  fields: {
-    dummyField: { type: GraphQLID },
-  },
 });
 
 module.exports = RootQueryType;
