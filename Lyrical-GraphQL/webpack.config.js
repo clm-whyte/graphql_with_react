8723e50025c93dfa2c0746fe30ff34cbcc3d7b73
ciw-administrate <ciw@getadministrate.com>
2020-05-14 16:59:32 +0100

    run prettier across the whole damn world

--- Lyrical-GraphQL/webpack.config.js
+++ Lyrical-GraphQL/webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: "./client/index.js",
   output: {
     path: "/",
