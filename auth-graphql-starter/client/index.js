79ed6fed2e9c0cdbac30e05e526cfd706d3baa7a
ciw-administrate <ciw@getadministrate.com>
2020-05-15 18:00:14 +0100

    query 'are you logged in' on load

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -4,11 +4,12 @@ import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
+import { Router, hashHistory, Route, IndexRoute } from "react-router";
+
+import App from "./components/App";
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: "http://localhost:4000/",
-});
+const link = new HttpLink();
 
 const client = new ApolloClient({
   cache,
@@ -19,7 +20,9 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Auth Starter</div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}></Route>
+      </Router>
     </ApolloProvider>
   );
 };
