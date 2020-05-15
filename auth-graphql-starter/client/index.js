d74be8bac764fa13b4edb9e1db547e13c554bc03
ciw-administrate <ciw@getadministrate.com>
2020-05-15 17:24:04 +0100

    configure to properly use cache

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -2,13 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
+import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://localhost:4000/",
+});
 
 const client = new ApolloClient({
+  cache,
+  link,
   dataIdFromObject: (o) => o.id,
 });
 
 const Root = () => {
-  return <div>Auth Starter</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>Auth Starter</div>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
