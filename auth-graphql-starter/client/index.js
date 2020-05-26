4a9e1e1ba2f6e20a3d9479c91fe5ef16f290f227
ciw-administrate <ciw@getadministrate.com>
2020-05-26 12:30:22 +0100

    quick hack to have the dashboard redirect unauthenticated users

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -10,6 +10,7 @@ import App from "./components/App";
 import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
 import Dashboard from "./components/Dashboard";
+import requireAuth from "./components/requireAuth";
 
 const cache = new InMemoryCache();
 const link = new HttpLink();
@@ -27,7 +28,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <Route path="login" component={LoginForm} />
           <Route path="signup" component={SignupForm} />
-          <Route path="dashboard" component={Dashboard} />
+          <Route path="dashboard" component={requireAuth(Dashboard)} />
         </Route>
       </Router>
     </ApolloProvider>
